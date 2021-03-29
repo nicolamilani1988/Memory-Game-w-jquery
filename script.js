35ef8dde366e03673c59046baab08f70a813bb4d
nicolamilani1988 <nicola.milani1988@outlook.it>
2021-03-29 10:08:22 +0200

    fixato bug sul click della stessa carta

--- script.js
+++ script.js
@@ -12,37 +12,32 @@ function selectCard(){
   var repeat = coppia+cardImg; // parametro cartaA / cartaB
 
 
-  if(control.includes(repeat)){ //condizione per evitare click sulla stessa carta
-    alert("cliccane un altra");
-  } else {
-    if(arr.length<1){ // creo array per matchare le immagini/testo
-      arr.push(cardImg);
-    } else if (arr.includes(cardImg)) { // carta matchata
-      // console.log("centro");
-      $(".my-card.show").each(function(){ // per ogni carta scoperta, faccio azioni
-        $(this).text("CARTA SCOPERTA");
-        $(this).addClass("show-strong"); // classe che mi permette di tenere display
-        $(this).parent().removeClass(".clickable"); // classe che mi rende incliccabile la carta
-      })
+
+  if(arr.length<1 || control.repeat<1){ // creo array per matchare le immagini/testo
+    arr.push(cardImg);
+    control.push(repeat);
+  } else if (arr.includes(cardImg) && control.indexOf(repeat) == -1) { // carta matchata
+    // console.log("centro");
+    $(".my-card.show").each(function(){ // per ogni carta scoperta, faccio azioni
+      $(this).text("CARTA SCOPERTA");
+      $(this).addClass("show-strong"); // classe che mi permette di tenere display
+      $(this).parent().removeClass(".clickable"); // classe che mi rende incliccabile la carta
+    })
+    arr=[]; //riazzero array per accoppiam
+    control=[]; // riazzero array per controllo
+
+  } else { // carta NON matchata
+    console.log("mancato");
+    $(".my-card.show").each(function(){
+      $(this).removeClass("show"); //rinascondo carta
       arr=[]; //riazzero array per accoppiam
       control=[]; // riazzero array per controllo
-
-    } else { // carta NON matchata
-      console.log("mancato");
-      $(".my-card.show").each(function(){
-        $(this).removeClass("show"); //rinascondo carta
-        arr=[]; //riazzero array per accoppiam
-        control=[]; // riazzero array per controllo
-        // --------CONTINUARE---------
-      })
-
-    }
+      // --------CONTINUARE---------
+    })
 
   }
   console.log("controllo match",arr);
   console.log("controllo AB" ,control);
-
-  control.push(repeat); //push controllo repeat che avviene solo alla prima carta,dopodichè ci sono condizioni per cui ilpush è stoppato in ogni caso.
 
 }
 
